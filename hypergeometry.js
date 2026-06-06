6d35ebfe9473e4b28fb98bc0227844f22ad023f3
BlnMn9 <nowakgrzegorz640@protonmail.com>
2026-06-06 02:26:50 +0200

    Update hypergeometry.js

--- hypergeometry.js
+++ hypergeometry.js
@@ -403,9 +403,9 @@ function loadDuocylinder() {
     buildThreeObjects();
 }
 
-// ZAKTUALIZOWANO: Większe sfery wierzchołków (0.025) dla lepszej widoczności
+// ZAKTUALIZOWANO: Zwiększono rozmiar sfer z 0.025 na 0.035 dla lepszej widoczności przy dużej skali
 function loadOkterakt() {
-    clearGeometry(0.025); 
+    clearGeometry(0.035); 
     for (let i = 0; i < 256; i++) {
         let x = (i & 1)   ? 0.55 : -0.55; let y = (i & 2)   ? 0.55 : -0.55;
         let z = (i & 4)   ? 0.55 : -0.55; let w = (i & 8)   ? 0.55 : -0.55;
@@ -413,6 +413,7 @@ function loadOkterakt() {
         let t = (i & 64)  ? 0.55 : -0.55; let s = (i & 128) ? 0.55 : -0.55;
         verticesND.push({ x: x, y: y, z: z, w: w, v: v, u: u, t: t, s: s });
     }
+    // ... reszta pętli generującej krawędzie pozostaje bez zmian ...
     for (let i = 0; i < 256; i++) {
         for (let j = i + 1; j < 256; j++) {
             let diff = 0;
@@ -429,6 +430,7 @@ function loadOkterakt() {
     }
     buildThreeObjects();
 }
+
 
 // --- PROJEKCJA I RENDEROWANIE ND -> 3D ---
 function projectND() {
